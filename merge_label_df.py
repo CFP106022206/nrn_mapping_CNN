0bcc8c708e6b28ee48150f023ea8b241edce4f38
Richard <avocado27315@gmail.com>
2023-12-22 01:20:01 +0800

    Update Sort rules

--- merge_label_df.py
+++ merge_label_df.py
@@ -13,13 +13,15 @@ df_lst = []
 
 for file_name in file_list:
     df = pd.read_csv(os.path.join(file_path, file_name))
-    # 针对df['fc_id']相同者, 用['model_predict']列从大到小排
-    df = df.sort_values(by=['fc_id', 'model_predict'], ascending=[True, False])
 
     df = df.drop(columns=['KT_score', 'binary_label'])
     df_lst.append(df)
 
 # 合并所有df
 df = pd.concat(df_lst, ignore_index=True)
+
+# 针对df['fc_id']相同者, 用['model_predict']列从大到小排
+df = df.sort_values(by=['fc_id', 'model_predict'], ascending=[True, False])
+
 df.to_csv('./result/unlabel_data_predict/merge_predict_Rank20.csv', index=False)
 # %%
