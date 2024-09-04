9b875cae56165ab4233847c5be70636ff25ed988
Richard <avocado27315@gmail.com>
2024-09-04 18:06:13 +0000

    使用更嚴格的threshold

--- main_draw.py
+++ main_draw.py
@@ -41,8 +41,8 @@ target_list = [tar]
 candidate_list = [can]
 threshold_of_exchange = 0.0  # threshold of considering the exchange of principal axes
 threshold_of_nI = 0.4#np.inf  # threshold of choosing pairs of neurons by normalized inertia of moment
-threshold_in = np.cos(np.pi*50/180)#np.cos(np.pi*90/180)  # threshold of inner product
-threshold_of_distance = 100#np.inf  # threshold of distance between wrapping EM data and FC data
+threshold_in = np.cos(np.pi*20/180)#np.cos(np.pi*90/180)  # threshold of inner product
+threshold_of_distance = 50#100#np.inf  # threshold of distance between wrapping EM data and FC data
 
 # Step 4 Score and rank the selected pairs
 cluster = False  # simplify the Strahler number
